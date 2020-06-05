3ece6a746f28eb36a1cb888c93c5e7be715c4042
long-hoang <thoanglong@gmail.com>
2020-06-05 07:24:24 +0000

    form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,20 @@
 class QuotesController < ApplicationController
   def index 
-
+    @quotes = Quote.all
   end
 
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+
+  private 
+  def quote_params
+    params.require(:quote).permit(:phrase, :author)
+  end
 end
